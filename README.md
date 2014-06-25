00739325953ad6f882bc408546e150e393ab249a
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-06-25 12:19:38 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mongoscope-statsd
+
+statsd config + backend for scope to push to which we can then use
+from scope to make more scopes scope.
---
+++ index.js
@@ -0,0 +1,8 @@
+{
+  mongoHost: 'localhost',
+  mongoPort: 27017,
+  mongoMax: 2160,
+  mongoPrefix: true,
+  mongoName: 'scope-stats',
+  backends: ['../mongo-statsd-backend/lib/index.js']
+}
